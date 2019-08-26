70e49648eb337203fd5da0bc857d1a59586f6be3
OsoreWatashi <2watashi@gmail.com>
2019-08-26 21:23:23 +0200

    Added first version of CORS support

--- api/Startup.cs
+++ api/Startup.cs
@@ -9,6 +9,9 @@ namespace PaganOnline.DataManager.API
 {
   public class Startup
   {
+    private const string DevelopmentCORS_Development = nameof(DevelopmentCORS_Development);
+    private const string DevelopmentCORS_Production = nameof(DevelopmentCORS_Production);
+    
     public Startup(IConfiguration configuration)
     {
       Configuration = configuration;
@@ -19,6 +22,11 @@ namespace PaganOnline.DataManager.API
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
+      services.AddCors(options =>
+        {
+          options.AddPolicy(DevelopmentCORS_Development, builder => { builder.WithOrigins("http://localhost:8080"); });
+          options.AddPolicy(DevelopmentCORS_Production, builder => { builder.WithOrigins("https://paganonline.wiki"); });
+        });
       services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
       services.AddSingleton(new DataManagerContext(Configuration.GetConnectionString("paganonline-datamanager")));
     }
@@ -29,11 +37,13 @@ namespace PaganOnline.DataManager.API
       if (env.IsDevelopment())
       {
         app.UseDeveloperExceptionPage();
+        app.UseCors(DevelopmentCORS_Development);
       }
       else
       {
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
+        app.UseCors(DevelopmentCORS_Production);
       }
 
       app.UseHttpsRedirection();
